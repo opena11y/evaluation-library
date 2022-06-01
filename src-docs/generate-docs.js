18b793fdb193cc68a9f7509b79fab78748a15969
Jon Gunderson <jongund@illinois.edu>
2022-05-31 20:15:49 -0500

    updated documentation

--- src-docs/generate-docs.js
+++ src-docs/generate-docs.js
@@ -14,10 +14,14 @@ import url   from 'url';
 
 import  nunjucks from 'nunjucks';
 
-import {allRules} from '../src/rules/allRules.js';
+import {
+  RULE_CATEGORIES,
+  WCAG_GUIDELINE
+}
+from '../src/constants.js'
 
 import {
- getBaseResultMessages,
+  getBaseResultMessages,
   getBaseResultMessage,
   getCommonMessage,
   getGuidelineInfo,
@@ -37,12 +41,50 @@ import {
   getTargetResourcesDesc,
   getTechniques,
   setLocale,
-  transformElementMarkup
+  transformElementMarkup,
+  setUseCodeTags
 } from '../src/_locale/locale.js';
+
+import {allRules} from '../src/rules/allRules.js';
+
+/* Constants */
 
 const outputDirectory = '../docs/';
 
-// Helper functions
+const ruleCategories = [
+  RULE_CATEGORIES.LANDMARKS,
+  RULE_CATEGORIES.HEADINGS,
+  RULE_CATEGORIES.STYLES_READABILITY,
+  RULE_CATEGORIES.IMAGES,
+  RULE_CATEGORIES.LINKS,
+  RULE_CATEGORIES.TABLES,
+  RULE_CATEGORIES.FORMS,
+  RULE_CATEGORIES.WIDGETS_SCRIPTS,
+  RULE_CATEGORIES.AUDIO_VIDEO,
+  RULE_CATEGORIES.KEYBOARD_SUPPORT,
+  RULE_CATEGORIES.TIMING,
+  RULE_CATEGORIES.SITE_NAVIGATION
+];
+
+const wcagGuidelines = [
+  WCAG_GUIDELINE.G_1_1,
+  WCAG_GUIDELINE.G_1_2,
+  WCAG_GUIDELINE.G_1_3,
+  WCAG_GUIDELINE.G_1_4,
+  WCAG_GUIDELINE.G_2_1,
+  WCAG_GUIDELINE.G_2_2,
+  WCAG_GUIDELINE.G_2_3,
+  WCAG_GUIDELINE.G_2_4,
+  WCAG_GUIDELINE.G_2_5,
+  WCAG_GUIDELINE.G_3_1,
+  WCAG_GUIDELINE.G_3_2,
+  WCAG_GUIDELINE.G_3_3,
+  WCAG_GUIDELINE.G_4_1
+]
+
+setUseCodeTags(true);
+
+/* Helper functions */
 
 function outputFile(fname, data) {
   fs.writeFile(path.join(outputDirectory, fname), data, err => {
@@ -53,28 +95,65 @@ function outputFile(fname, data) {
   })
 }
 
+function getRulesByCategory(rc) {
 
-const allRuleInfo = [];
+  const rules = [];
+
+  allRules.forEach( rule => {
+
+    if (rule.rule_category_id & rc) {
+      const ruleInfo = {};
+      const id = rule.rule_id;
+
+      ruleInfo.id            = getRuleId(id);
+      ruleInfo.filename      = 'rule-' + rule.rule_id.toLowerCase().replace('_', '-') + '.html';
+      ruleInfo.last_updated  = rule.last_updated;
+
+      ruleInfo.ruleset          = getRulesetInfo(rule.ruleset);
+      ruleInfo.rule_scope       = rule.rule_scope;
+      ruleInfo.rule_category    = getRuleCategoryInfo(rule.rule_category_id);
+      ruleInfo.rule_category_id = rule.rule_category_id;
+      ruleInfo.conformance      = rule.rule_required ? getCommonMessage('required') : getCommonMessage('recommended');
+
+      ruleInfo.primary_id = rule.wcag_primary_id;
+      ruleInfo.primary    = getSuccessCriterionInfo(rule.wcag_primary_id);
+      ruleInfo.related    = getSuccessCriteriaInfo(rule.wcag_related_ids);
+
+      ruleInfo.target_resources = rule.target_resources;
+
+      ruleInfo.definition = getRuleDefinition(id);
+      ruleInfo.summary    = getRuleSummary(id);
+      ruleInfo.purposes   = getPurposes(id);
+
+      ruleInfo.techniques = getTechniques(id);
+      ruleInfo.information_links = getInformationLinks(id);
+
+      rules.push(ruleInfo);
+    }
+  });
+
+  return rules;
+}
+
+const allRulesInfo = getRulesByCategory(RULE_CATEGORIES.ALL);
+
+const allRuleCategories = [];
 
 // Create data for creating index and rule files
 
-allRules.forEach( rule => {
-  const ruleInfo = {};
-  const id = rule.rule_id;
 
-  ruleInfo.id         = getRuleId(id);
-  ruleInfo.filename   = 'rule-' + rule.rule_id.toLowerCase().replace('_', '-') + '.html';
-  ruleInfo.definition = getRuleDefinition(id);
-  ruleInfo.summary    = getRuleSummary(id);
-  ruleInfo.primary_id = rule.wcag_primary_id;
-  ruleInfo.primary    = getSuccessCriterionInfo(rule.wcag_primary_id);
+ruleCategories.forEach( rc => {
+  const rcInfo = getRuleCategoryInfo(rc);
+  rcInfo.rules = getRulesByCategory(rc);
 
-  allRuleInfo.push(ruleInfo);
+  console.log(`[RC]: ${rcInfo.title} (${rcInfo.rules.length})`);
+
+  allRuleCategories.push(rcInfo);
 });
 
 // Create index file
 
-const htmlIndex = nunjucks.render('./templates/index.njk', {title: 'Rules', allRuleInfo: allRuleInfo});
+const htmlIndex = nunjucks.render('./templates/index.njk', {title: 'Rules by Rule Category', allRuleCategories: allRuleCategories});
 outputFile('index.html', htmlIndex);
 
 // Create apis file
@@ -87,14 +166,9 @@ outputFile('about.html', htmlAbout);
 
 
 // Create rule files
-
-allRuleInfo.forEach( ruleInfo => {
-  console.log(ruleInfo.summary);
+console.log(`\n === Rule Files ===`)
+allRulesInfo.forEach( ruleInfo => {
+  console.log(`${ruleInfo.summary} => ${ruleInfo.filename}`);
   const htmlRule = nunjucks.render('./templates/rule.njk', {title: ruleInfo.summary, ruleInfo: ruleInfo});
-  fs.writeFile(path.join(outputDirectory, ruleInfo.filename), htmlRule, err => {
-      if (err) {
-        console.error(err)
-        return
-      }
-  });
+  outputFile(ruleInfo.filename, htmlRule);
 });
